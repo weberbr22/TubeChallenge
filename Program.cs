e9d568a36dd3f69545299f2306dec467e3b12635
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2022-11-08 21:55:27 +0000

    ff

--- Program.cs
+++ Program.cs
@@ -2,11 +2,80 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 
 namespace TubeChallenge
 {
     class Program
     {
+        static void Task1()
+        {
+            try
+            {
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                docPath = Path.Combine(docPath, "Academic/Computing/C#/unsorted.txt");
+
+                StreamWriter myFile = new StreamWriter(docPath);
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Random rnd = new Random();
+                    int num = rnd.Next(50);
+                    myFile.WriteLine(num);
+                }
+
+                string docPath1 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                docPath1 = Path.Combine(docPath1, "Academic/Computing/C#/sorted.txt");
+
+                StreamReader input = new StreamReader(docPath);
+                StreamWriter output = new StreamWriter(docPath1);
+
+
+                int[] inter = new int[3];
+
+                string line = string.Empty;
+                int i = 0;
+                do
+                {
+                    line = input.ReadLine();
+                    if (line != null)
+                    {
+                        Console.WriteLine(line);
+                        inter[i] = Int32.Parse(line);
+                        i += 1;
+                    }
+
+                } while (line != null);
+
+                int temp = 0;
+
+                for (int write = 0; write < inter.Length; write++)
+                {
+                    for (int sort = 0; sort < inter.Length - 1; sort++)
+                    {
+                        if (inter[sort] > inter[sort + 1])
+                        {
+                            temp = inter[sort + 1];
+                            inter[sort + 1] = inter[sort];
+                            inter[sort] = temp;
+                        }
+                    }
+                    Console.Write("{0} ", inter[write]);
+                }
+
+                output.Write(string.Join(", ", inter));
+
+            }
+
+            catch(IOException e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
         static bool Shares(string s1, string s2)
         {
             bool[] v = new bool[126];
@@ -80,7 +149,8 @@ namespace TubeChallenge
 
         static void Main(string[] args)
         {
-            Tubes();
+            Task1();
+            //Tubes();
             //Console.WriteLine(Shares("St John's Wood", "Mackerel"));
             Console.WriteLine("Hello World!");
         }
